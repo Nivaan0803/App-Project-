8b2038761e67dbd44c5cc349056fe5461b2f935b
Arthur <ar.roschal@gmail.com>
2026-04-14 22:02:46 -0400

    Workflow Instructions

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import streamlit as st
 #
+# Upgrade Python package manager:
 # python.exe -m pip install --upgrade pip
+#
+# Install all the required libraries:
 # pip install -r requirements.txt
+#
+# Run the streamlit program (instead of green triangle print this in the terminal)
 # streamlit run main.py
 #
+
+""" Draft code"""
+
 st.title("Cognitive Support App") # think of a good title
 st.header("Daily Check-in")
 mood = st.slider("How do you feel today?", 1, 5)
